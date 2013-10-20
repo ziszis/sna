12e9f356fa478159ecfb4ae090cc30eff3ecfade
Ivan Zauharodneu <zis.zis@gmail.com>
2013-10-21 01:11:21 +0200

    refactor

--- src/com/zis/sna/Parse.scala
+++ src/com/zis/sna/Parse.scala
@@ -1,42 +1,45 @@
 import io.Source
 import java.io.File
-import java.util.Date
 
 case class Card(name: String, edition: String)
-case class Point(date: Date, price: Int)
+case class Point(card: Card, buy: Int, sell: Int, volume: Int)
 
 object Parser {
-  def file(filename: String) : Iterator[(Card, Int, Int, Int)] = {
-    val content = Source.fromFile(filename, "ISO-8859-1")
+  private val volumeRegexp = """.*\[(\d+)\]""".r
+  private val cardRegexp = """(.*) \[(.*)\]""".r
+
+  def file(filename: String) : Iterator[Point] = {
+    def parseCard(s: String) = s.trim match { case cardRegexp(name, edition) => Card(name, edition) }
+    def parsePrice(s: String) = (s.trim.toFloat * 1000.0).toInt
+    def parseVolume(s: String) = s.trim.split(" ").map { case volumeRegexp(x) => x.toInt }.sum
+
+    Source.fromFile(filename, "ISO-8859-1")
       .getLines()
       .dropWhile(!_.startsWith("==="))
       .filterNot(s => s.isEmpty || s.startsWith("==="))
-
-    def parseCard(s: String) = {
-      val p = """(.*) \[(.*)\]""".r
-      s.trim match { case p(name, edition) => Card(name, edition) }
-    }
-    def parsePrice(s: String) = (s.trim.toFloat * 1000.0).toInt
-    def parseVolume(s: String) = {
-      val p = """.*\[(\d+)\]""".r
-      s.trim.split(" ").map(_ match { case p(x) => x.toInt }).sum
-    }
-
-    content.map(_.split("   +").toList match {
-      case List(a, b, c, d) => (parseCard(a), parsePrice(b), parsePrice(c), parseVolume(d))
-      case List(a, c, d) => (parseCard(a), 0, parsePrice(c), parseVolume(d))
-      case List(a, b) => (parseCard(a), parsePrice(b), 0, 0)
-      case List(a) => (parseCard(a), 0, 0, 0)
-      case s => throw new RuntimeException("Failed to parse " + s)
+      .map(_.split("   +").toList match {
+        case List(a, b, c, d) => Point(parseCard(a), parsePrice(b), parsePrice(c), parseVolume(d))
+        case List(a, c, d) => Point(parseCard(a), 0, parsePrice(c), parseVolume(d))
+        case List(a, b) => Point(parseCard(a), parsePrice(b), 0, 0)
+        case List(a) => Point(parseCard(a), 0, 0, 0)
+        case s => throw new RuntimeException("Failed to parse " + s)
     })
   }
 }
 
 object Parse extends App {
   val root = new File("/Users/zis/Desktop/supernova")
-  for (f <- root.list) {
-    val ps = Parser.file(root.getAbsolutePath + "/" + f)
-    val vind = ps.filter(p => p._1.name == "Vindicate" && p._1.edition == "AP" && p._3 != 0)
-    if (!vind.isEmpty) println(f + "\t" + vind.map(_._3).min)
+  def selectCard(name: String, edition: String) =
+    root.list.flatMap(filename => {
+      Parser.file(root.getAbsolutePath + "/" + filename).filter(p => p.card.name == name && p.card.edition == edition)
+    })
+  def avg(pts:Seq[Int]) = {
+    val x = pts.map((_, 1)).reduce((a, b) => (a._1+b._1, a._2+b._2))
+    x._1 / x._2
   }
+  def avgSell(cards: Seq[Point]) = cards.filter(_.volume != 0).map(_.sell)
+  val start = System.currentTimeMillis()
+  println(avg(selectCard("Vindicate", "AP").filter(_.volume != 0).map(_.sell)))
+  val end = System.currentTimeMillis()
+  println(end - start)
 }
